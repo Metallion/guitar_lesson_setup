179b9d036badd3c68b5ea632504955025c210a96
Metallion <idontcheckthisfoo@gmail.com>
2022-08-07 13:42:10 +0900

    Added a script to connect the mixers to zoom instead of skype

---
+++ lesson_setup/pipewire_connections/zoom.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pw-link "mixer_andreas:Master L" "ZOOM VoiceEngine:input_FL"
+pw-link "mixer_andreas:Master R" "ZOOM VoiceEngine:input_FR"
+pw-link "ZOOM VoiceEngine:output_FL" "mixer_itani:Itani Voice L"
+pw-link "ZOOM VoiceEngine:output_FR" "mixer_itani:Itani Voice R"
