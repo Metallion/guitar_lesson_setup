8d332ed5a2ae323121bada780b79711d1f52ef57
Metallion <idontcheckthisfoo@gmail.com>
2022-10-28 15:34:27 +0900

    Allow me to pass an argument to the pipewire script to select which video call software we're using.

--- pipewire_connections.sh
+++ pipewire_connections.sh
@@ -2,6 +2,8 @@
 
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
+video_call_software="${1:-skype}"
+
 "${SCRIPT_DIR}/lesson_setup/pipewire_connections/main.sh"
 
 # Mics
@@ -10,6 +12,7 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 "${SCRIPT_DIR}/lesson_setup/pipewire_connections/mics/scarlett.sh"
 
 # Video call
+"${SCRIPT_DIR}/lesson_setup/pipewire_connections/video_call_softwares/${video_call_software}.sh"
 #"${SCRIPT_DIR}/lesson_setup/pipewire_connections/video_call_softwares/zoom.sh"
-"${SCRIPT_DIR}/lesson_setup/pipewire_connections/video_call_softwares/skype.sh"
+#"${SCRIPT_DIR}/lesson_setup/pipewire_connections/video_call_softwares/skype.sh"
 #"${SCRIPT_DIR}/lesson_setup/pipewire_connections/video_call_softwares/google_meet.sh"
