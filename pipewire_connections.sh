1f659cf7881cef5c7636298f359c48a90a5066a8
Metallion <idontcheckthisfoo@gmail.com>
2022-10-28 15:56:49 +0900

    Made the mic selectable with commandline arguments too

--- pipewire_connections.sh
+++ pipewire_connections.sh
@@ -3,16 +3,8 @@
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 video_call_software="${1:-skype}"
+mic="${2:-scarlett}"
 
 "${SCRIPT_DIR}/lesson_setup/pipewire_connections/main.sh"
-
-# Mics
-#"${SCRIPT_DIR}/lesson_setup/pipewire_connections/mics/webcam.sh"
-#"${SCRIPT_DIR}/lesson_setup/pipewire_connections/mics/onboard.sh"
-"${SCRIPT_DIR}/lesson_setup/pipewire_connections/mics/scarlett.sh"
-
-# Video call
+"${SCRIPT_DIR}/lesson_setup/pipewire_connections/mics/${mic}.sh"
 "${SCRIPT_DIR}/lesson_setup/pipewire_connections/video_call_softwares/${video_call_software}.sh"
-#"${SCRIPT_DIR}/lesson_setup/pipewire_connections/video_call_softwares/zoom.sh"
-#"${SCRIPT_DIR}/lesson_setup/pipewire_connections/video_call_softwares/skype.sh"
-#"${SCRIPT_DIR}/lesson_setup/pipewire_connections/video_call_softwares/google_meet.sh"
